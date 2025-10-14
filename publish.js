d11ac1a04ed3bc3d6dd560b698015975fa1ed799
swagc2 <taibaidded1122@gmail.com>
2025-10-14 21:01:15 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'lbtjoljn.xyz',
+	'mbkfgox.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
